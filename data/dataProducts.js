55c8b471b9094c6295a3d9c0da2b48b623908f72
nguyenvantrang2901 <nguyenvantrang2901@gmail.com>
2024-06-01 23:59:30 +0700

    code detail product

--- data/dataProducts.js
+++ data/dataProducts.js
@@ -2,8 +2,9 @@ export const ProductsData = [
     {
         _id: 1,
         name: 'Iphone 15',
-        desc: 'Tăng độ cứng cáp và tối ưu khối lượng với chất liệu Titan',
+        desc: `Tăng độ cứng cáp và tối ưu khối lượng với chất liệu TitanThe iPhone 15, released in September 2023, continues Apple's tradition of innovation and design excellence. Here are some key features and specifications`,
         price: '1500$',
+        quantity: 30,
         category: 'mobile',
         imageUrl: 'https://m.media-amazon.com/images/I/71bErtQPC3L._SL1500_.jpg'
    },
@@ -12,6 +13,7 @@ export const ProductsData = [
         name: 'Laptop ASUS Vivobook 14 X1404ZA-NK389W',
         desc: '(I7-1255U/16GB/512GB PCIE/14.0 FHD/WIN11/BẠC) - Chính hãng',
         price: '1500$',
+        quantity: 10,
         category: 'desktop',
         imageUrl: 'https://m.media-amazon.com/images/I/71bErtQPC3L._SL1500_.jpg'
     },
--- screens/ProductDetail.js
+++ screens/ProductDetail.js
@@ -1,44 +1,125 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import {ProductsData} from "../data/dataProducts"
+import { TouchableOpacity } from 'react-native'
+import Layout from "../components/Layout/Layout"
 
-const ProductDetail = () => {
+const ProductDetail = ({route}) => {
+  const [proDetail, setProDetail] = useState({})
+  const [qty, setQty] = useState(2)
+
+  useEffect(()=>{
+    // find product detail
+    const getProduct = ProductsData.find((p)=>{
+      return p._id = params._id
+    })
+    setProDetail(getProduct)
+  },[params?._id])
+
+  const {params} = route
+  // console.log(route)
+
+  const handleIncreQty = () => {
+    // if(qty> 5)
+    setQty(pre=> pre+1)
+  }
+  const handleDecreQty = () => {
+    if(qty<=1) return
+    setQty(pre=>pre-1)
+  }
+  const handlerAddToCart = () => {
+    alert("Thêm vào giỏ hàng thành công")
+  }
   return (
-    <View style={styles.container}>
-      <View style={styles.half1}>
-        {/* Các phần tử trong phần 1 */}
-        <View style={styles.innerElement}></View>
-        <View style={styles.innerElement}></View>
+    <Layout >
+      <Image style={styles.image} source={{uri: proDetail?.imageUrl}}/>
+      <View style={styles.container}>
+
+        <Text style={styles.title}>{proDetail?.name}</Text>
+        <Text style={styles.title}>Price : {proDetail?.price} VND</Text>
+        <Text style={styles.price}>{proDetail?.desc}</Text>
+
+        <View style={styles.btnContainer}>
+          <TouchableOpacity 
+            onPress={handlerAddToCart}
+            style={styles.btnCart}>
+            <Text style={styles.btnText}>Add to cart</Text>
+          </TouchableOpacity>
+
+          <View style={styles.btnContainer}>
+            <TouchableOpacity 
+              onPress={handleDecreQty}
+              style={styles.btnQty}>
+              <Text style={styles.qtyText}>-</Text>
+            </TouchableOpacity>
+            <Text style={styles.btnQty}>{qty}</Text>
+            <TouchableOpacity 
+              onPress={handleIncreQty}
+              style={styles.btnQty}>
+              <Text style={styles.qtyText}>+</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
       </View>
-      <View style={styles.half2}>
-        {/* Các phần tử trong phần 2 */}
-        <View style={styles.innerElement}></View>
-        <View style={styles.innerElement}></View>
-        <View style={styles.innerElement}></View>
-      </View>
-    </View>
+    </Layout>
   )
 }
 
 export default ProductDetail
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-  },
-  half1: {
-    flex: 1,
-    backgroundColor: 'red',
-    padding: 10,
-  },
-  half2: {
-    flex: 1,
-    backgroundColor: 'blue',
-    padding: 10,
-  },
-  innerElement: {
-    height: 50,
-    marginBottom: 10,
-    backgroundColor: 'white',
-  },
+    image : {
+      height: 300,
+      width: '100%',
+      marginTop: 10
+    },
+    container: {
+      marginVertical: 15,
+      marginHorizontal: 10
+    },
+    title: {
+      fontSize: 18,
+      textAlign: 'left'
+    },
+    desc: {
+      fontSize: 13,
+      textTransform: 'capitalize',
+      textAlign: 'center',
+      marginVertical: 10
+    },
+    btnContainer: {
+      flexDirection: 'row',
+      marginHorizontal: 10,
+      marginVertical: 20,
+      alignItems: 'center',
+      justifyContent: 'center',
+      gap: 5
+    },
+    btnCart: {
+      width: 140,
+      height: 30,
+      borderWidth: 1,
+      borderColor: 'gray',
+      backgroundColor: 'orange',
+      borderRadius: 5,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    btnText: {
+      flexSize: 18,
+      fontWeight: 'bold'
+    },
+    btnQty: {
+      width: 30,
+      borderWidth: 1,
+      borderColor: 'gray',
+      borderRadius: 5,
+      alignItems:'center',
+      justifyContent: 'center',
+      textAlign: 'center',
+    },
+    qtyText: {
+      fontSize: 15
+    }
 })
